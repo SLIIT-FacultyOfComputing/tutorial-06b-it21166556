395eadb0aebc29d5fed42b6638334ae4592a06ef
I.U.N.Nadeeshan <89896805+it21166556@users.noreply.github.com>
2022-03-15 04:41:06 +0000

    I have completed Tutorial 6 Exercise 01

--- Student.cpp
+++ Student.cpp
@@ -1,12 +1,17 @@
 #include "Student.h"
 #include <iostream>
+#include <string.h>
+using namespace std;
 
 // Assign studentId and name
-Student::assignDetails() {
-  
+void Student::assignDetails(int pId, const char pname[20]){
+  studentId = pId;
+  strcpy(name,pname);
 }
 
 // Display StudentId and Name
-Student::display() {
-  
+void Student::display(){
+  cout << "Student ID is " << studentId << "\n";
+  cout << "Student name is " << name << "\n";
 }
+
